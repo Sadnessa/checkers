14f20c891258e33724ca7be957b117fe50248367
Darya Volkova <anrich@bk.ru>
2021-11-29 20:31:04 +0200

    changed ChHeader

--- src/components/ChHeader.vue
+++ src/components/ChHeader.vue
@@ -1,12 +1,27 @@
 <template>
-<div></div>
+<div class="ch-header">
+  <div class="col col--30">dsdsd</div>  
+  <div class="col col--50">dsddsds</div>
+</div>
 </template>
 
-<script>
-export default {
-  name: 'ChHeader',
+<style lang="scss" scoped>
+.ch-header {
+  display: flex;
+  justify-content: center;
+  width: 100%;
 }
-</script>
 
-<style scoped>
+.col {
+  &--30 {
+width: 30%;
+margin-right: 10px;
+background: chartreuse;
+  }
+
+  &--50 {
+width: 50%;
+background: cadetblue;
+  }
+}
 </style>
