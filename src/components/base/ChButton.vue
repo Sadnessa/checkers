534a87e96864d28882547df207208d56fca5038b
Darya Volkova <anrich@bk.ru>
2021-11-29 22:14:06 +0200

    changed ChButton

--- src/components/base/ChButton.vue
+++ src/components/base/ChButton.vue
@@ -1,24 +1,43 @@
 <template>
-    <button :style="{background: color}">
-        <slot name="left"></slot>
-        <slot></slot>
-        <slot name="right"></slot>
-    </button>
+  <button :style="{ background: color }">
+    <div class="left">
+      <slot name="left"></slot>
+    </div>
+    <slot></slot>
+    <div class="right">
+      <slot name="right"></slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
-    props: {
-        color: {
-            type: String,
-            default: "",
-        }
-    }
-}
+  props: {
+    color: {
+      type: String,
+      default: "",
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 button {
-    display: flex;
+  display: flex;
+  align-items: center;
+  padding: 6px;
+  border-radius: 5px;
+  border: none;
+  margin: 12px 0px;
+}
+
+.left {
+  display: flex;
+  margin-right: 4px;
+}
+
+.right {
+  display: flex;
+  margin-left: 4px;
 }
 </style>
