95614b4976f606753fb179c8f3316ef52f34f2a6
Darya Volkova <anrich@bk.ru>
2021-11-29 21:37:28 +0200

    changed font style

--- src/App.vue
+++ src/App.vue
@@ -29,9 +29,13 @@ export default {
 };
 </script>
 
-<style>
+<style> 
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@500;700&family=Rubik:wght@400;500&display=swap');
+ * {
+   font-family: 'Roboto', sans-serif;
+   }
+   
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   display: flex;
   justify-content: center;
   background: blue;
