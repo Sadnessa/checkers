bbd659e2e48f4612de249faf722a8baa9d35b175
Darya Volkova <anrich@bk.ru>
2021-11-30 19:21:56 +0200

    changed ChFeatures

--- src/components/ChFeatures.vue
+++ src/components/ChFeatures.vue
@@ -1,7 +1,69 @@
 <template>
-    <div></div>
+  <div class="features-section">
+    <div class="title">
+      <ChText large>Features</ChText>
+    </div>
+    <div class="card-wrapper">
+      <ChCard v-for="card in cards" :key="card.text">
+        <template #img>
+          <img :src="(card.imgUrl)" heigh="120" width="120" />
+        </template>
+        <ChText textColor="black">
+            {{card.text}}
+        </ChText>
+      </ChCard>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<script>
+import ChText from "./base/ChText.vue"
+import ChCard from "./base/ChCard.vue";
 
+export default {
+    data() {
+        return {
+            cards: [
+                {
+                    imgUrl: require("../assets/sobaka.png"),
+                    text: "Play in any chat"
+                },
+                {
+                    imgUrl: require("../assets/lang.png"),
+                    text: "Multi language"
+                },
+                {
+                    imgUrl: require("../assets/cake.png"),
+                    text: "Game skins"
+                },
+                {
+                    imgUrl: require("../assets/free.png"),
+                    text: "No ads"
+                },
+                {
+                    imgUrl: require("../assets/rules.png"),
+                    text: "Custom rules"
+                },
+                {
+                    imgUrl: require("../assets/git.png"),
+                    text: "OpenSource"
+                },
+            ]
+        }
+    },
+    components: {
+        ChText,
+        ChCard,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-wrapper {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    max-width: 80%;
+    justify-content: center;
+}
 </style>
