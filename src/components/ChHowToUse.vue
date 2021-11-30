52b6f83cd2d857a70fa0c3b9cbda9c2d9fc7c3b9
Darya Volkova <anrich@bk.ru>
2021-11-30 18:17:09 +0200

    changed ChHowToUse

--- src/components/ChHowToUse.vue
+++ src/components/ChHowToUse.vue
@@ -1,7 +1,43 @@
 <template>
-    <div></div>
+  <div class="section">
+    <div class="text-wrapper">
+      <ChText large>How to use</ChText>
+      <ChText textColor="#979797">After /start command in @GameCheckersBot you can call bot by typing its username and a query in the text input field 
+in any chat. </ChText>
+    </div>
+    <div class="image-wrapper">
+    <img src="../assets/phone_image.png" />
+  </div>
+  </div>
+  
 </template>
 
-<style lang="scss" scoped>
+<script>
+import ChText from "./base/ChText.vue";
 
+export default {
+  components: {
+    ChText,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.section {
+  display: flex;
+  justify-content: space-between;
+  padding: 30px 0px;
+}
+
+.text-wrapper {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    p {
+        max-width: 60%;
+    }
+}
 </style>
