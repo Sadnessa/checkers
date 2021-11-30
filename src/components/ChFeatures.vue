9364780e65eafd10cb264e6dac33c828421f04a4
Darya Volkova <anrich@bk.ru>
2021-11-30 20:47:59 +0200

    changed ChFooter

--- src/components/ChFeatures.vue
+++ src/components/ChFeatures.vue
@@ -6,10 +6,10 @@
     <div class="card-wrapper">
       <ChCard v-for="card in cards" :key="card.text">
         <template #img>
-          <img :src="(card.imgUrl)" heigh="120" width="120" />
+          <img :src="card.imgUrl" heigh="120" width="120" />
         </template>
         <ChText textColor="black">
-            {{card.text}}
+          {{ card.text }}
         </ChText>
       </ChCard>
     </div>
@@ -17,65 +17,65 @@
 </template>
 
 <script>
-import ChText from "./base/ChText.vue"
+import ChText from "./base/ChText.vue";
 import ChCard from "./base/ChCard.vue";
 
 export default {
-    data() {
-        return {
-            cards: [
-                {
-                    imgUrl: require("../assets/sobaka.png"),
-                    text: "Play in any chat"
-                },
-                {
-                    imgUrl: require("../assets/lang.png"),
-                    text: "Multi language"
-                },
-                {
-                    imgUrl: require("../assets/cake.png"),
-                    text: "Game skins"
-                },
-                {
-                    imgUrl: require("../assets/free.png"),
-                    text: "No ads"
-                },
-                {
-                    imgUrl: require("../assets/rules.png"),
-                    text: "Custom rules"
-                },
-                {
-                    imgUrl: require("../assets/git.png"),
-                    text: "OpenSource"
-                },
-            ]
-        }
-    },
-    components: {
-        ChText,
-        ChCard,
-    }
-}
+  data() {
+    return {
+      cards: [
+        {
+          imgUrl: require("../assets/sobaka.png"),
+          text: "Play in any chat",
+        },
+        {
+          imgUrl: require("../assets/lang.png"),
+          text: "Multi language",
+        },
+        {
+          imgUrl: require("../assets/cake.png"),
+          text: "Game skins",
+        },
+        {
+          imgUrl: require("../assets/free.png"),
+          text: "No ads",
+        },
+        {
+          imgUrl: require("../assets/rules.png"),
+          text: "Custom rules",
+        },
+        {
+          imgUrl: require("../assets/git.png"),
+          text: "OpenSource",
+        },
+      ],
+    };
+  },
+  components: {
+    ChText,
+    ChCard,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .features-section {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    margin: 30px 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 30px 0px;
 }
 .card-wrapper {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    max-width: 80%;
-    justify-content: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  max-width: 80%;
+  justify-content: center;
 
-    p {
-        font-weight: bold;
-        font-size: 18px;
-    }
+  p {
+    font-weight: bold;
+    font-size: 18px;
+  }
 }
 </style>
--- src/components/ChFooter.vue
+++ src/components/ChFooter.vue
@@ -1,7 +1,49 @@
 <template>
-    <div></div>
+  <div class="footer-section">
+    <ChButton color="#FFFFFF">
+      <template #left>
+        <img src="../assets/git.png" height="48" width="50" />
+      </template>
+      <ChText textColor="#3F3F3F"> Create issue </ChText>
+    </ChButton>
+    <ChButton color="#FFFFFF">
+      <template #left>
+        <img src="../assets/git.png" height="48" width="50" />
+      </template>
+      <ChText textColor="#3F3F3F"> Create language request </ChText>
+    </ChButton>
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<script>
+import ChButton from "./base/ChButton.vue";
+import ChText from "./base/ChText.vue";
 
+export default {
+  components: {
+    ChButton,
+    ChText,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.footer-section {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 80px;
+}
+
+button {
+  border: 2px solid #3f3f3f;
+  box-sizing: border-box;
+  border-radius: 5px;
+  margin: 6px;
+}
+
+p {
+  font-weight: bold;
+  font-size: 18px;
+}
 </style>
