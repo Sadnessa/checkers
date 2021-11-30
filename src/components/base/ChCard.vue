f9b50223a19f20f3ffc8a109dcbcf0dfd108c7f3
Darya Volkova <anrich@bk.ru>
2021-11-30 19:21:43 +0200

    changed ChCard

--- src/components/base/ChCard.vue
+++ src/components/base/ChCard.vue
@@ -1,9 +1,29 @@
 <template>
-    <div class="card-wrapper">
-
+  <div class="card-section">
+    <div class="img-wrapper">
+      <slot name="img"></slot>
     </div>
+    <div class="text-wrapper">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+.card-section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  border-radius: 5px;
+  box-shadow: 0px 4px 11px 3px rgba(0, 0, 0, 0.12);
+  padding: 0px;
+  margin: 16px 10px;
+}
 
+.text-wrapper {
+  margin-top: 20px;
+}
 </style>
