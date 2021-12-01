175195d6a272103036dddbfcf1444b067da9222b
Darya Volkova <anrich@bk.ru>
2021-12-01 13:19:44 +0200

    changed ChSuppSection

--- src/components/ChSuppSection.vue
+++ src/components/ChSuppSection.vue
@@ -1,9 +1,48 @@
 <template>
-    <div class="supp-section">
-        
+  <div class="supp-section">
+    <div class="header">
+      <ChText large> Support </ChText>
+      <img src="../assets/supp.png" heigth="187" width="183" />
     </div>
+    <div class="status-bar">
+        <ChText textColor="#979797">
+            I have to pay for the bot server every month. You can help me.
+        </ChText>
+    </div>
+    <div class="button-wrapper"></div>
+  </div>
 </template>
 
-<style lang="scss" scoped>
+<script>
+import ChText from "./base/ChText.vue";
 
+export default {
+  components: {
+    ChText,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.supp-section {
+  margin-top: 80px;
+
+  .header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+      position: absolute;
+      left: 60%;
+    }
+  }
+
+  .status-bar {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-top: 86px;
+  }
+}
 </style>
