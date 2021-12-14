104f8dccee6fc0230679b5e84906a5bd156ab864
Darya Volkova <anrich@bk.ru>
2021-12-14 11:10:50 +0200

    Added some fixes

--- src/components/base/ChCard.vue
+++ src/components/base/ChCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card-section">
-    <div class="img-wrapper">
+    <div class="img-wrapper" v-if="$slots.img">
       <slot name="img"></slot>
     </div>
     <div class="text-wrapper">
@@ -15,15 +15,9 @@
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 200px;
-  height: 200px;
   border-radius: 5px;
- box-shadow: 0px 4px 6px -2px rgba(0, 0, 0, 0.51);
+  box-shadow: 0px 4px 6px -2px rgba(0, 0, 0, 0.51);
   padding: 0px;
   margin: 16px 10px;
-}
-
-.text-wrapper {
-  margin-top: 20px;
 }
 </style>
